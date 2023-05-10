28d60fea78610eac31a08d0816ed5193a449775a
kaios <kaios@itmchannelmarketing.com>
2023-05-10 18:01:26 -0300

    projeto de exemplo de spring mvc com jsp

---
+++ SpringMVCExample/src/main/java/com/journaldev/spring/model/User.java
@@ -0,0 +1,14 @@
+package com.journaldev.spring.model;
+
+public class User {
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
